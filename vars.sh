8c3db4dd69409a641d37d3b70fc9e81ca871e7ea
Sol Boucher <slb1566@rit.edu>
2012-12-05 01:06:57 -0500

    Fixed locking issue occurring when all refs are rejected
    In such a case, the post-receive hook was never executed, so we never
    actually cleared our lockfile.  Thus, no one else was ever allowed to
    push after a single offender had been completely abandoned.

--- vars.sh
+++ vars.sh
@@ -1,2 +1,3 @@
 adapterpath="../bridge" #path to adapter (non-bare, non-working) Git repo
 lockfile="svn-dcommit.lock" #unique SVN lockfile in gits/guts of adapter repo
+totalextension="total" #suffix to use for separate total ref count lock
